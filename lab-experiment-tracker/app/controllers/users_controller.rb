1b33cb21a75234b51027e57ea21bbfa71632e286
Kavya R <email": null>
2017-03-29 16:57:55 -0700

    Fix params require

--- lab-experiment-tracker/app/controllers/users_controller.rb
+++ lab-experiment-tracker/app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
  private
 
    def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:first_name, :last_name, :isScientist, :email, :password)
     end
 end
