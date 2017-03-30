ad4bc5c64b28aae48d77d4b044fbfab5858dff47
Kavya R <email": null>
2017-03-29 17:02:17 -0700

    Adds resources projects, routes for sessions/user & root route

--- lab-experiment-tracker/config/routes.rb
+++ lab-experiment-tracker/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :projects
+
+  root 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
 end
