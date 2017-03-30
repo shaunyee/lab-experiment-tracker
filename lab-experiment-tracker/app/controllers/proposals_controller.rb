1a0cb6093bc390f406bd19645028711b213ff560
“Vilde <vilde@vevatne.no>
2017-03-30 12:05:30 -0700

    Adds routes to proposal_controller

--- lab-experiment-tracker/app/controllers/proposals_controller.rb
+++ lab-experiment-tracker/app/controllers/proposals_controller.rb
@@ -3,4 +3,22 @@ class ProposalsController < ApplicationController
   def index
   end
 
+  def new
+    @proposal = Proposal.new
+  end
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+
+    if @proposal.save
+      redirect_to @proposal, notice: 'Proposal was successfully created.'
+    else
+      render :new, status 422
+    end 
+  end
+
+private
+  def proposal_params
+    params.require(:proposal).permit(:summary, :hypothesis)
+  end
 end
