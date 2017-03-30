6d9aafdd89fd98071d43d1b21b692542b51715b6
Marcela <marcela.fporto@gmail.com>
2017-03-29 19:26:20 -0700

    Updates migration to polymorphic

--- lab-experiment-tracker/db/migrate/20170329220941_create_comments.rb
+++ lab-experiment-tracker/db/migrate/20170329220941_create_comments.rb
@@ -3,7 +3,8 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.string :body, :null => false
       t.integer :user_id, :null => false
-      t.integer :project_id, :null => false
+      t.integer :commentable_id
+      t.string  :commentable_type
       t.timestamps
     end
   end
--- lab-experiment-tracker/db/migrate/20170329220947_create_observations.rb
+++ lab-experiment-tracker/db/migrate/20170329220947_create_observations.rb
@@ -3,7 +3,8 @@ class CreateObservations < ActiveRecord::Migration[5.0]
     create_table :observations do |t|
       t.string :body, :null => false
       t.integer :user_id, :null => false
-      t.integer :project_id, :null => false
+      t.integer :observational_id
+      t.string  :observational_type
       t.timestamps
     end
   end
