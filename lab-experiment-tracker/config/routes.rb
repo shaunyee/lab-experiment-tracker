943f893025aae1d38d8dde731ea0f178e4056866
Marcela <marcela.fporto@gmail.com>
2017-03-29 19:21:47 -0700

    Update root routes

--- lab-experiment-tracker/config/routes.rb
+++ lab-experiment-tracker/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :projects
 
-  root 'projects#index'
+  root 'proposals#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/signup' => 'users#new'
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  
+
 end
