d3f43d6ba0b2753ca5ac662e143d24a54d0092db
krammohan <kavya.ramm@gmail.com>
2017-03-30 15:22:45 -0700

    Adds partials to index and show pages for models

--- lab-experiment-tracker/app/controllers/proposals_controller.rb
+++ lab-experiment-tracker/app/controllers/proposals_controller.rb
@@ -1,6 +1,16 @@
 class ProposalsController < ApplicationController
 
   def index
+  	@proposals = Proposal.all
+  end
+
+  def new
+  	@proposal = Proposal.new
+  end
+
+  def create
+  	@proposal = Proposal.create(proposal_params)
+  	redirect_to '/'
   end
 
 end
