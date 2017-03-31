2a28bc9fa09f8b4c96aaced2e062464b44acc2fd
Marcela <marcela.fporto@gmail.com>
2017-03-31 12:30:32 -0700

    fixes equal in get_status

--- lab-experiment-tracker/app/models/proposal.rb
+++ lab-experiment-tracker/app/models/proposal.rb
@@ -4,13 +4,13 @@ class Proposal < ApplicationRecord
   has_many :comments, as: :commentable
 
 def get_status
-  if self.status = 1
+  if self.status == 1
     "open"
-  elsif self.status = 2
+  elsif self.status == 2
     "in progress"
-  elsif self.status = 3
+  elsif self.status == 3
     "closed"
   else
     "archived"
-  end    
+  end
 end
