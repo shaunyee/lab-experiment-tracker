e4d3be44d9cae88b8e9e435149450f4068a9fa02
Marcela <marcela.fporto@gmail.com>
2017-03-31 12:30:01 -0700

    adds get_status method to proposal model

--- lab-experiment-tracker/app/models/proposal.rb
+++ lab-experiment-tracker/app/models/proposal.rb
@@ -2,4 +2,15 @@ class Proposal < ApplicationRecord
   belongs_to :user
   has_many :experiments
   has_many :comments, as: :commentable
+
+def get_status
+  if self.status = 1
+    "open"
+  elsif self.status = 2
+    "in progress"
+  elsif self.status = 3
+    "closed"
+  else
+    "archived"
+  end    
 end
