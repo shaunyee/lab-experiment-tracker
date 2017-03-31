492202b622ed9a300393b81ebb575379c92d5908
Marcela <marcela.fporto@gmail.com>
2017-03-31 14:06:29 -0700

    Adds status

--- lab-experiment-tracker/app/models/proposal.rb
+++ lab-experiment-tracker/app/models/proposal.rb
@@ -3,14 +3,16 @@ class Proposal < ApplicationRecord
   has_many :experiments
   has_many :comments, as: :commentable
 
-def get_status
-  if self.status == 1
-    "open"
-  elsif self.status == 2
-    "in progress"
-  elsif self.status == 3
-    "closed"
-  else
-    "archived"
+  def get_status
+    if self.status == 1
+      "open"
+    elsif self.status == 2
+      "in progress"
+    elsif self.status == 3
+      "closed"
+    else
+      "archived"
+    end
   end
+  
 end
